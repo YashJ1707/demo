f6026c3f5e0aed3282fd1f1b6dd02b413d416777
Yash Jaybhaye <78841896+YashJ1707@users.noreply.github.com>
2021-08-16 14:29:52 +0530

    commit on 'domonew'

--- first.c
+++ first.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+int fibbo(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    return fibbo(n - 1) + fibbo(n - 2);
+}
 int main(int argc, char const *argv[])
 {
-    printf("This is my first code");
+    int n;
+    printf("Enter length of series\n");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", fibbo(i));
+    }
     return 0;
 }
